0edf55c48fc1c8f0c83d4ecea94c877be75ccf2d
Dheeraj Nampally <nampally@motivationfactory.com>
2022-02-20 10:12:13 +0100

    Add project files.

---
+++ MiddleWare/Authentification.cs
@@ -0,0 +1,34 @@
+﻿namespace Middleware
+{
+    public class Authentification
+    {
+        private readonly RequestDelegate _next;
+        private const string Api_Key = "x-api-key";
+        public Authentification(RequestDelegate next)
+        {
+            _next = next;
+        }
+        public async Task InvokeAsync(HttpContext context)
+        {
+            if (!context.Request.Headers.TryGetValue(Api_Key, out var extractedApiKey))
+            {
+                context.Response.StatusCode = 401;
+                await context.Response.WriteAsync("Api Key was not provided");
+                return;
+            }
+
+            var appSettings = context.RequestServices.GetRequiredService<IConfiguration>();
+
+            var apiKey = appSettings.GetValue<string>(Api_Key);
+
+            if (!apiKey.Equals(extractedApiKey))
+            {
+                context.Response.StatusCode = 401;
+                await context.Response.WriteAsync("Unauthorized client - Please contact administrator");
+                return;
+            }
+
+            await _next(context);
+        }
+    }
+}
